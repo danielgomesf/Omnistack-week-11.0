2a48036cd5eb32e68f8016e94971ea53ff6200b5
Daniel Gomes Falcão <danielgomesfalcao@gmail.com>
2020-03-28 20:48:51 -0300

    Mobile finalizado

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -1,21 +1,23 @@
 const connection = require('../database/connection');
 
 module.exports = {
-    async index(request, response) { 
+    async index(request, response) {
         const { page = 1 } = request.query;
 
         const [count] = await connection('incidents').count();
 
         const incidents = await connection('incidents')
-            .join('ongs', 'ong_id', '=', 'incidents.ong_id')
-            .limit(5)
-            .offset((page - 1) * 5)
-            .select(['incidents.*', 
-                'ongs.name', 
-                'ongs.email', 
-                'ongs.whatsapp',
-                'ongs.city', 
-                'ongs.uf']);
+              .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
+              .limit(5)
+              .offset((page - 1) * 5)
+              .select([
+                  'incidents.*',
+                  'ongs.name',
+                  'ongs.email',
+                  'ongs.whatsapp',
+                  'ongs.city',
+                  'ongs.uf',
+              ]);
 
         response.header('X-Total-Count', count['count(*)']);
 
@@ -30,7 +32,7 @@ module.exports = {
             title,
             description,
             value,
-            ong_id
+            ong_id,
         });
 
         return response.json({ id });
